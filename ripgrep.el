876d9b410f9a183ab6bbba8fa2b9e1eb79f3f7d2
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2017-01-16 09:47:59 +0100

    Update package documentation

--- ripgrep.el
+++ ripgrep.el
@@ -9,8 +9,17 @@
 
 ;;; Commentary:
 
-;; Please see README.md for documentation, or read it online at
-;; https://github.com/nlamirault/ripgrep.el
+;; Emacs front-end for ripgrep, a command line search tool
+
+;; Installation:
+
+;; ripgrep.el is available on the two major community maintained repositories
+;; Melpa stable (https://stable.melpa.org), and Melpa (https://melpa.org)
+;;
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/") t)
+;;
+;; M-x package-install ripgrep
 
 ;;; License:
 
